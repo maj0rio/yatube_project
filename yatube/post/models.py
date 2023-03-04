c580e914c87fbf10928683ab3366a3c4062f2788
Ваня Фадеев <fadeevvanya@gmail.com>
2023-03-05 01:12:47 +0300

    Create models, admin-site, some css.

--- yatube/post/models.py
+++ yatube/post/models.py
@@ -1,12 +1,22 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from datetime import datetime
+
 
 User = get_user_model()
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=150)
-    text = models.TextField()
+    title = models.CharField('Заголовок', max_length=100)
+    text = models.TextField('Текст')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    date = models.DateTimeField('Дата публикации',default=datetime.now, blank=True)
+    class Meta:
+        ordering = ["-date"]
+
+    def __str__(self):
+        return self.title
+
+
 
 
